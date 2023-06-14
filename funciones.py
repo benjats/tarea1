6afdc214e415f8ffb02ede24fb9505d6571afbee
benjats <benjamintobar2006@gmail.com>
2023-06-13 21:06:04 -0400

    agregue la funcion para verificar si es primo y lo prober altiro con un print y un return

--- funciones.py
+++ funciones.py
@@ -1,2 +1,14 @@
-print("holaa, como estas?")
-print("hidhfh")
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+# Ejemplo de uso
+numero = int(input("Ingrese un número: "))
+if es_primo(numero):
+    print(numero, "es un número primo.")
+else:
+    print(numero, "no es un número primo.")
